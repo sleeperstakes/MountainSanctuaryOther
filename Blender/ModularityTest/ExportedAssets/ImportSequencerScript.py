987b37a183c6cf214108d469fb528f36e2f20641
sleeperstakes <charliecrome0@gmail.com>
2026-06-01 20:59:08 +0100

    Naming and Door Size Changes

---
+++ Blender/ModularityTest/ExportedAssets/ImportSequencerScript.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: 2018-2025 Xavier Loux (BleuRaven)
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# ----------------------------------------------
+#  Blender For UnrealEngine
+#  https://github.com/xavier150/Blender-For-UnrealEngine-Addons
+# ----------------------------------------------
+
+# This script was generated with the addons Unreal Engine Assets Exporter.
+# This script should be run in Unreal Engine to import into Unreal Engine 4 and 5 assets.
+# The assets are exported from from Unreal Engine Assets Exporter. More detail here. https://github.com/xavier150/Blender-For-UnrealEngine-Addons
+# Use the following command in Unreal Engine cmd consol to import sequencer: 
+# py "[ScriptLocation]\sequencer_import_script.py"
+
+import importlib
+import importlib.util
+import os
+import sys
+import json
+
+def JsonLoad(json_file):
+    # Changed in Python 3.9: The keyword argument encoding has been removed.
+    if sys.version_info >= (3, 9):
+        return json.load(json_file)
+    else:
+        return json.load(json_file, encoding="utf8")
+
+
+def JsonLoadFile(json_file_path):
+    if sys.version_info[0] < 3:
+        with open(json_file_path, "r") as json_file:
+            return JsonLoad(json_file)
+    else:
+        with open(json_file_path, "r", encoding="utf8") as json_file:
+            return JsonLoad(json_file)
+
+def RunImportScriptWithJsonData():
+    # Prepare process import
+    json_data_file = 'ImportSequencerData.json'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    import_file_path = os.path.join(dir_path, json_data_file)
+    sequence_data = JsonLoadFile(import_file_path)
+    
+    file_path = os.path.join(sequence_data["info"]["addon_path"],'run_unreal_import_script.py')
+    spec = importlib.util.spec_from_file_location("__import_sequencer__", file_path)
+    module = importlib.util.module_from_spec(spec)
+
+    # Run script module function
+    spec.loader.exec_module(module)
+    module.run_from_sequencer_import_script(import_file_path)
+
+if __name__ == "__main__":
+    RunImportScriptWithJsonData()
